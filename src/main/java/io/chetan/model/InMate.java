cfd66f2e4a9ab9570da70e94020da46738fde02e
CHETAN <chetu.is@gmail.com>
2020-05-29 21:33:54 +0530

    implemENTED addinmate() & viewinmate

--- src/main/java/io/chetan/model/InMate.java
+++ src/main/java/io/chetan/model/InMate.java
@@ -17,7 +17,6 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Pattern;
 
-import org.apache.tomcat.jni.Address;
 import org.springframework.format.annotation.DateTimeFormat;
 
 
@@ -45,7 +44,7 @@ public class InMate implements Comparable<InMate>
 	
 	private String aadhaarNumber ;
 	
-	@Embedded
+	//@Embedded
 	private Address address;
 	
 	private String occupation ;
